1661de6d2e8abffba079cbdad34b3f6f9cb7f13e
ningueim <136115006+ningueim@users.noreply.github.com>
2023-07-13 00:52:59 -0300

    Create App.py

---
+++ App.py
@@ -0,0 +1,11 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+st.title('Web App Football Data')
+
+st.sidebar.header("Leagues")
+
+st.sidebar.header("Season")
+
+#Webscrapping Football Data 
